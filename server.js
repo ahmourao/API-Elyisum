c698bfb1e12f30581a347ec0b832c3c43ce99147
Ana Moura <fon2018ana@gmail.com>
2023-11-04 18:31:57 -0300

    nova estrutura de diretórios e arqvs

--- server.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app.js";
-
-const PORT = 3000; 
-
-app.listen(PORT, () => {
-    //listen é um evento
-    console.log(chalk.black.bgBlue("Servidor está escutando!"));
-});
--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
-import chalk from "chalk";
+
 // import dotenv from 'dotenv';
 // dotenv.config();
+import chalk from "chalk";
 import express from "express";
+import { PrismaClient } from '@prisma/client'
+const PORT = 3000; 
 
 const app = express(); //instância do express
+const prisma = new PrismaClient()
 
-export default app;
+app.use(express.json())
+app.listen(PORT, () => {
+    //listen é um evento
+    console.log(chalk.black.bgBlue("Servidor está escutando!"));
+});
